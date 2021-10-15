74de4de0889263f22891355b1780f9d3a20c7c37
rani1029 <ranig1029@gmail.com>
2021-10-16 02:34:02 +0530

    unittesting done for calculating fare

---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using CabInvoiceGeneratorApp;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        InvoiceGenerator invoiceGenerator = null;
+
+        [Test]
+        public void GivenDistanceAndTimeShouldReturnTotalFare()
+        {
+            //Creating Instance of Invoice Generator  For Normal Ride
+            invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double distance = 2.0;
+            int time = 5;
+
+            //Calculating Fare
+            double fare = invoiceGenerator.CalculateFare(distance, time);
+            double expected = 25;
+
+            //Asserting Values
+            Assert.AreEqual(expected, fare);
+        }
+    }
+}
